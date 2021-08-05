a77b6de46849c9fbd3972bbeb30c143245b2b686
costa-e <ehrensberger.c@me.com>
2021-08-05 21:33:36 +0200

    print debug travis1

--- testing_support/BaseIntegrationTest.py
+++ testing_support/BaseIntegrationTest.py
@@ -14,7 +14,12 @@ class BaseIntegrationTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         cta_root_path = Path(__file__).parent.parent.resolve()
-        print('at teardown:', Path(__file__).parent.parent.resolve())
+        print('at teardown:', cta_root_path)
+
+        for f in os.listdir(cta_root_path):
+            print('child:', f)
+            print('full:', os.path.join(self.destination_root, f))
+
         full_path = cta_root_path / Path(self.destination_root)
         for f in os.listdir(full_path):
             os.remove(os.path.join(self.destination_root, f))
