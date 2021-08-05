8e09dd017344d4d0965a0e43042d912755783d67
costa-e <ehrensberger.c@me.com>
2021-08-05 18:22:34 +0200

    add hardcoded data

--- analysers/PreAnalyser.py
+++ analysers/PreAnalyser.py
@@ -11,5 +11,14 @@ class PreAnalyser(BaseAnalyser):
         It analyses the project in self.project_root_path.
         :return: list of named tuples WarningItem, one WarningItem for each warning in the project
         '''
-        #TODO Yifei: change this
-        return []
+        # TODO Yifei: change this to actual logic
+        warning = WarningItem(project_name="test_project",
+                              warning_code="DPCT1111",
+                              file_path='/kernel_wrapper2.dp.cpp',
+                              message="DPCT1111:3: The workgroup size passed to the SYCL kernel may exceed the limit.\n"
+                                      "To get the device limit, query info::device::max_work_group_size. Adjust the\n"
+                                      "workgroup size if needed.",
+                              line=127)
+        all_warnings = [warning]
+
+        return all_warnings
