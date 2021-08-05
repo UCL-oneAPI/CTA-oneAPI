4bbc20bd91587f1684988e5d747907d947924963
costa-e <ehrensberger.c@me.com>
2021-08-05 21:17:51 +0200

    make system agnostic paths

--- testing_support/BaseIntegrationTest.py
+++ testing_support/BaseIntegrationTest.py
@@ -6,11 +6,11 @@ from pathlib import Path
 class BaseIntegrationTest(unittest.TestCase):
     @property
     def destination_root(self):
-        return 'testing_support/integration_testing_data/destination_dir'
+        return Path.joinpath(Path('testing_support'), 'integration_testing_data', 'destination_dir')
 
     @property
     def dpct_root(self):
-        return 'testing_support/integration_testing_data/test_project'
+        return Path.joinpath(Path('testing_support'), 'integration_testing_data', 'test_project')
 
     def tearDown(self) -> None:
         cta_root_path = Path(__file__).parent.parent.resolve()
