d6926324f48d2b3c5efb713a90c17a4ab6d961e2
costa-e <ehrensberger.c@me.com>
2021-08-05 21:55:52 +0200

    print debug travis2

--- testing_support/BaseIntegrationTest.py
+++ testing_support/BaseIntegrationTest.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import unittest
 from pathlib import Path
 
@@ -18,7 +19,22 @@ class BaseIntegrationTest(unittest.TestCase):
 
         for f in os.listdir(cta_root_path):
             print('child:', f)
-            print('full:', os.path.join(self.destination_root, f))
+            print('child full:', os.path.join(cta_root_path, f))
+            child = os.path.join(cta_root_path, f)
+            if os.path.isdir(child):
+                for g in os.listdir(child):
+                    grandchild = os.path.join(child, g)
+                    print('child child:', g)
+                    print('child child full:', grandchild)
+                    if os.path.isdir(grandchild):
+                        for h in os.listdir(grandchild):
+                            ggchild = os.path.join(grandchild, h)
+                            print('child child child:', h)
+                            print('child child child full:', ggchild)
+                    else:
+                        print(f'grandchild is not directory: {grandchild}')
+            else:
+                print(f'child is not directory: {child}')
 
         full_path = cta_root_path / Path(self.destination_root)
         for f in os.listdir(full_path):
