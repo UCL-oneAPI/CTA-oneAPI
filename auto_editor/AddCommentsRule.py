50c6564dc2fd9dbd0456fb1a33a35b13305ef3d0
zhangQC1 <457375273@qq.com>
2021-08-09 21:33:19 +0100

    Modified InsertComments 1

--- auto_editor/AddCommentsRule.py
+++ auto_editor/AddCommentsRule.py
@@ -3,6 +3,7 @@ from typing import List
 from auto_editor.BaseRule import BaseRule
 from auto_editor.StructuredProjectSource import StructuredProjectSource
 from enums import ChangeTypeEnum
+from auto_editor.LineItem import LineItem
 
 
 class AddCommentsRule(BaseRule):
@@ -13,51 +14,53 @@ class AddCommentsRule(BaseRule):
     @property
     def dpct_warning_codes(self) -> List[str]:
         # Todo: add relevant warning codes
-        return ['DPCT1111','DPCT1065']
+        # Add new warning types in this list
+        return ['DPCT1049','DPCT1065']
 
     def run_rule(self, project: StructuredProjectSource,
                  warning_first_line: int, warning_last_line: int, file_path: str) -> StructuredProjectSource:
         # Todo: add rule here
-        # print(f'rule runs here. warning first: {warning_first_line}')
-        # return project
-
-        state = 0
+        state = False
         w_type = "DPCT"
-        new_line_items = list()
-
         all_line = list()
-        for l in project.get_line_items(file_path):
-            all_line.append(l.code)
+        all_items = project.paths_to_lines[file_path]
+        for c in all_items:
+            all_line.append(c.code)
 
         for i in range(all_line.__len__()):
             line_list = list(all_line[i].split())
-            if state == 1:
-
-                # Add if statement for new warning types
-
+            if state == True:
                 if len(line_list) != 0 and line_list[0].split(':')[0] in self.dpct_warning_codes:
                     w_type = line_list[0].split(':')[0]
 
-            if len(line_list) != 0 and line_list[:1][0] == '/*':
-                state = 1
-            if state == 1 and len(line_list) != 0 and line_list[-1:][0] == '*/':
-                state = 0
+            if len(line_list) != 0 and line_list[0] == '/*':
+                state = True
+            if state == True and len(line_list) != 0 and line_list[-1] == '*/':
+                state = False
 
                 # add if statement for the corresponding warning type, and change the recommendation
 
-                if w_type == "DPCT1111":
-                    all_line.insert(int(i) + 1, "/*it is works!*/\n")
+                if w_type == "DPCT1049":
+                    del all_items[warning_first_line: warning_last_line + 1]
+                    comment_item = LineItem("/*This is an insertion in the code!*/\n")
+                    all_items.insert(warning_first_line, comment_item)
                     w_type = "DPCT"
 
+
+
                 if w_type == "DPCT1065":
-                    all_line.insert(int(i) + 1, "/*it is also works!*/\n")
+                    del all_items[warning_first_line: warning_last_line + 1]
+                    comment_item = LineItem("/*It works!*/\n")
+                    all_items.insert(warning_first_line, comment_item)
                     w_type = "DPCT"
 
         fp_file = open(file_path, 'w+')
-        for item in all_line:
+        all_items = project.paths_to_lines[file_path]
+        new_lines = list()
+        for c in all_items:
+            new_lines.append(c.code)
+        for item in new_lines:
             fp_file.write(item)
         fp_file.close()
 
-        project.paths_to_lines = project.get_paths_to_lines()
-        project.dpct_warnings_dict = project.get_dpct_warnings_dict()
         return project
