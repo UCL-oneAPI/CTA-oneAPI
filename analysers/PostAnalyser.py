dc7a5a3de1fb64db06bc5950ec31d91f1d04d423
zhongyuanli <zczlz91@ucl.ac.uk>
2021-08-11 09:45:06 +0800

    add get_all_warnings()

--- analysers/PostAnalyser.py
+++ analysers/PostAnalyser.py
@@ -2,4 +2,29 @@ from analysers.BaseAnalyser import BaseAnalyser
 
 
 class PostAnalyser(BaseAnalyser):
-    pass
+    def get_all_warnings(self) -> List[WarningItem]:
+        project = StructuredProjectSource(self.project_root_path)
+        warnings_dict = project.dpct_warnings_dict
+        all_warnings = []
+        codes = []
+        ids = []
+        for i in project.paths_to_lines.values():
+            for j in i:
+                codes.append(j.code)
+                ids.append(j.id)
+
+        for k, v in warnings_dict.items():
+            for info in v:
+                path = '/' + info[2]
+                first_line = self.get_first_line_num(info[0], codes, ids)
+                message = self.get_warning_message(first_line, info[1], codes, ids)
+                warning = WarningItem(project_name=self.project_root_path.stem,
+                                      warning_code=k,
+                                      file_path=path,
+                                      message=message,
+                                      # "DPCT1111:3: The workgroup size passed to the SYCL kernel may exceed the limit.\n"
+                                      #         "To get the device limit, query info::device::max_work_group_size. Adjust the\n"
+                                      #         "workgroup size if needed.",
+                                      line=first_line)
+                all_warnings.append(warning)
+        return all_warnings
