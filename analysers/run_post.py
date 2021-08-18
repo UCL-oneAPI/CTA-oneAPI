85e994baa49f1bd21ccc2d93f6f5b8bcd7919bcc
zhongyuanli <zczlz91@ucl.ac.uk>
2021-08-18 10:07:10 +0800

    bug fix

--- analysers/run_post.py
+++ analysers/run_post.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def run():
     cta_root = Path(__file__).parent.parent.resolve()
-    p = Path.joinpath(cta_root, 'auto_editor', 'sample_data', 'test_project')
+    p = Path.joinpath(cta_root, 'auto_editor', 'sample_data', 'destination_dir')
     t = StructuredProjectSource_Recommendation(p)
     # t = StructuredProjectSource(p)
     print(t)
