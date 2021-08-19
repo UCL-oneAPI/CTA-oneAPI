289c17f4acb24065efdf9b166e3952f2b853bd61
Yifei Zhao <ucaby46@ucl.ac.uk>
2021-08-19 20:50:42 +0800

    update presenter

--- report_presentation/Presenter.py
+++ report_presentation/Presenter.py
@@ -17,10 +17,6 @@ class Presenter:
         pass
 
     def html_page(self):
-        # cta_path = Path(__file__).parent.parent.resolve()
-        # dpct_root = Path.joinpath(cta_path, 'testing_support', 'integration_testing_data', 'test_project')
-        # preAnalyser = PreAnalyser(dpct_root)
-        # all_warnings = PreAnalyser.get_all_warnings(preAnalyser)
         html = '''
                 <html lang="en">
                 <head>
@@ -52,12 +48,11 @@ class Presenter:
                   {background-color: #F2F2F2;}
                 
                 </style>
-                <body><ul>
-                <li>Total Number of Warnings: %s</li>
-                <p>
-                  <img src="before-histogram.jpg" width="400" height="300" />
+                <body>
+                <p>Total Number of Warnings: %s</p>
+                  <img src="before-histogram.jpg" width="600" height="450" />
                 </p>
-                <li>Detailed Warning Information</li>
+                <p>Detailed Warning Information</p>
                 <div class = "before-warning-table">
                 <table border = "0">
                         <tr>
@@ -86,42 +81,39 @@ class Presenter:
         html += '''
                 </table>
                 </div>
-                </ul>
                 </body>
                 </html>
                 '''
         return html
 
+    def visualization(self, warnings):
+        warning_codes = []
+        file_codes = {}
+        files_contain_warnings = {}
+        for i in warnings:
+            warning_codes.append(i.warning_code)
+            file_codes.setdefault(i.warning_code, []).append(i.file_path)
+        occurrence = Counter(warning_codes)
+        print(occurrence)
 
-def visualization(warnings):
-    warning_codes = []
-    file_codes = {}
-    files_contain_warnings = {}
-    for i in warnings:
-        warning_codes.append(i.warning_code)
-        file_codes.setdefault(i.warning_code, []).append(i.file_path)
-    occurrence = Counter(warning_codes)
-    print(occurrence)
+        for k, v in file_codes.items():
+            files_contain_warnings[k] = len(list(set(v)))
 
-    for k, v in file_codes.items():
-        files_contain_warnings[k] = len(list(set(v)))
-
-    x = np.arange(len(occurrence.keys()))
-    bar_width = 0.2
-    plt.figure(2)
-    #plt.bar(occurrence.keys(), occurrence.values(), 0.2, color='blue', alpha=0.8)
-    plt.bar(x, occurrence.values(), bar_width, align="center", color="green", label="Number of Occurrences", alpha=0.5)
-    plt.bar(x + bar_width, files_contain_warnings.values(), bar_width, align="center", color="blue", label="Number of documents containing such warnings",
-            alpha=0.5)
-    plt.xticks(x + bar_width / 2, occurrence.keys())
-    #plt.xlabel("X-axis")
-    plt.ylabel("Occurrence")
-    plt.yticks(np.arange(0, max(occurrence.values())+1, step=3))
-    #plt.title("Warning Types with Number of Occurrences")
-    plt.legend(loc="upper left", fontsize="x-small")
-    plt.savefig('before-histogram.jpg')
-    plt.show()
-
+        x = np.arange(len(occurrence.keys()))
+        bar_width = 0.2
+        plt.figure(2)
+        #plt.bar(occurrence.keys(), occurrence.values(), 0.2, color='blue', alpha=0.8)
+        plt.bar(x, occurrence.values(), bar_width, align="center", color="green", label="Number of Occurrences", alpha=0.5)
+        plt.bar(x + bar_width, files_contain_warnings.values(), bar_width, align="center", color="blue", label="Number of documents containing such warnings",
+                alpha=0.5)
+        plt.xticks(x + bar_width / 2, occurrence.keys())
+        #plt.xlabel("X-axis")
+        plt.ylabel("Occurrence")
+        plt.yticks(np.arange(0, max(occurrence.values())+1, step=3))
+        #plt.title("Warning Types with Number of Occurrences")
+        plt.legend(loc="upper left", fontsize="x-small")
+        plt.savefig('before-histogram.jpg')
+        plt.show()
 
 
 report_root = Path(__file__).parent
@@ -132,6 +124,6 @@ all_warnings = PreAnalyser.get_all_warnings(preAnalyser)
 final_warnings = []
 changes = []
 presenter = Presenter(report_root, all_warnings, final_warnings, changes)
-visualization(all_warnings)
+presenter.visualization(all_warnings)
 with open('test.html', 'w') as f:
     f.write(presenter.html_page())
