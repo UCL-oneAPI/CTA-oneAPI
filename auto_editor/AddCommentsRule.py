93524e01e40d3276663a0e79bb0968496a86f9ce
zhangQC1 <457375273@qq.com>
2021-08-20 09:00:30 +0100

    add recommendation

--- auto_editor/AddCommentsRule.py
+++ auto_editor/AddCommentsRule.py
@@ -15,7 +15,7 @@ class AddCommentsRule(BaseRule):
     def dpct_warning_codes(self) -> List[str]:
         # Todo: add relevant warning codes
         # Add new warning types in this list
-        return ['DPCT1049','DPCT1065']
+        return ['DPCT1065','DPCT1039','DPCT1008','DPCT1000','DPCT1032','DPCT1001']
 
     def run_rule(self, project: StructuredProjectSource,
                  warning_first_line: int, warning_last_line: int, file_path: str) -> StructuredProjectSource:
@@ -39,17 +39,44 @@ class AddCommentsRule(BaseRule):
                 state = False
 
                 # add if statement for the corresponding warning type, and change the recommendation
-
-                if w_type == "DPCT1049":
+                if w_type == "DPCT1065":
                     del all_items[warning_first_line - 1: warning_last_line]
-                    comment_item = LineItem("/*CTA1049:count number: recommendation*/\n")
+                    comment_item = LineItem("/*CTA1065:count number: recommended to ignore this warning. but you can also consider replacing 'item_ct1.barrier();' with 'item_ct1.barrier(sycl::access::fence_space::local_space);'*/\n")
                     all_items.insert(warning_first_line - 1, comment_item)
                     w_type = "DPCT"
                     return project
 
-                if w_type == "DPCT1065":
+                if w_type == "DPCT1039":
                     del all_items[warning_first_line - 1: warning_last_line]
-                    comment_item = LineItem("/*CTA1065:count number: recommended to ignore this warning. but you can also consider replacing 'item_ct1.barrier();' with 'item_ct1.barrier(sycl::access::fence_space::local_space);'*/\n")
+                    comment_item = LineItem("/*CTA1039:count number: Base on the experience, strongly recommended to leave the code as it is and ignore this warning. BUT, if '&xxx' points to local memory, please accept the DPCT proposal.*/\n")
+                    all_items.insert(warning_first_line - 1, comment_item)
+                    w_type = "DPCT"
+                    return project
+
+                if w_type == "DPCT1008":
+                    del all_items[warning_first_line - 1: warning_last_line]
+                    comment_item = LineItem("/*CTA1008:count number:Please accept the DPCT proposal.  You can leave the code as it is for now.*/\n")
+                    all_items.insert(warning_first_line - 1, comment_item)
+                    w_type = "DPCT"
+                    return project
+
+                if w_type == "DPCT1000":
+                    del all_items[warning_first_line - 1: warning_last_line]
+                    comment_item = LineItem("/*CTA1000:count number: Base on the experience, strongly recommended to ignore this warning.*/\n")
+                    all_items.insert(warning_first_line - 1, comment_item)
+                    w_type = "DPCT"
+                    return project
+
+                if w_type == "DPCT1001":
+                    del all_items[warning_first_line - 1: warning_last_line]
+                    comment_item = LineItem("/*CTA1001:count number: Base on the experience, strongly recommended to ignore this warning.*/\n")
+                    all_items.insert(warning_first_line - 1, comment_item)
+                    w_type = "DPCT"
+                    return project
+
+                if w_type == "DPCT1032":
+                    del all_items[warning_first_line - 1: warning_last_line]
+                    comment_item = LineItem("/*CTA1032:count number: Base on the experience, recommended to ignore this warning. If it didn't work, adjust the code.*/\n")
                     all_items.insert(warning_first_line - 1, comment_item)
                     w_type = "DPCT"
                     return project
