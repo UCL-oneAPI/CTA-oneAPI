2eefbb848b779d5837acf99aa0053a08da1c1f82
Yifei Zhao <77520578+Kritt430@users.noreply.github.com>
2021-08-22 22:48:44 +0800

    Update PostAnalyser.py

--- analysers/PostAnalyser.py
+++ analysers/PostAnalyser.py
@@ -13,30 +13,42 @@ class PostAnalyser(BaseAnalyser):
     so I recommend working with a 'dummy' string here.
     E.g. for the ticket you can assume that all CTA warnings/recommendations begin with "CTA<some-number>: "
     """
+
     def get_all_warnings(self) -> List[WarningItem]:
+        '''
+        This creates the data for the report of dpct warnings.
+        It analyses the project in self.project_root_path.
+        :return: list of named tuples WarningItem, one WarningItem for each warning in the project
+        '''
+
         project = StructuredProjectSource(self.project_root_path)
         warnings_dict = project.dpct_warnings_dict
         all_warnings = []
-        codes = []
-        ids = []
+        all_codes = {}
+        all_ids = {}
 
-        for i in project.paths_to_lines.values():
-            for j in i:
-                codes.append(j.code)
-                ids.append(j.id)
+        for name, line_item in project.paths_to_lines.items():
+            for i in line_item:
+                all_codes.setdefault(name, []).append(i.code)
+                all_ids.setdefault(name, []).append(i.id)
 
         for k, v in warnings_dict.items():
             for info in v:
                 path = '/' + info[2]
-                first_line = self.get_first_line_num(info[0], codes, ids)
-                message = self.get_warning_message(first_line, info[1], codes, ids)
-                #cta_number, dpct_number = self.count_warnings_numbers(k,cta_number,dpct_number)
-                warning = WarningItem(project_name=self.project_root_path.stem,
-                                      warning_code=k,
-                                      file_path=path,
-                                      message=message,
-                                      line=first_line)
-                all_warnings.append(warning)
+                if info[2] in all_ids.keys():
+                    codes = all_codes[info[2]]
+                    ids = all_ids[info[2]]
+                    first_line = self.get_first_line_num(info[0], codes, ids)
+                    message = self.get_warning_message(first_line, info[1], codes, ids)
+                    warning = WarningItem(project_name=self.project_root_path.stem,
+                                          warning_code=k,
+                                          file_path=path,
+                                          message=message,
+                                          # "DPCT1111:3: The workgroup size passed to the SYCL kernel may exceed the limit.\n"
+                                          #         "To get the device limit, query info::device::max_work_group_size. Adjust the\n"
+                                          #         "workgroup size if needed.",
+                                          line=first_line)
+                    all_warnings.append(warning)
         return all_warnings
 
 
@@ -51,24 +63,27 @@ class PostAnalyser(BaseAnalyser):
         project = StructuredProjectSource_Recommendation(self.project_root_path)
         recommendations_dict = project.recommendations_dict
         all_recommendations = []
-        codes = []
-        ids = []
+        all_codes = {}
+        all_ids = {}
 
-        for i in project.paths_to_lines.values():
-            for j in i:
-                codes.append(j.code)
-                ids.append(j.id)
+        for name, line_item in project.paths_to_lines.items():
+            for i in line_item:
+                all_codes.setdefault(name, []).append(i.code)
+                all_ids.setdefault(name, []).append(i.id)
 
         for k, v in recommendations_dict.items():
             for info in v:
                 path = '/' + info[2]
-                first_line = self.get_first_line_num(info[0], codes, ids)
-                message = self.get_warning_message(first_line, info[1], codes, ids)
-                warning = RecommendationItem(project_name=self.project_root_path.stem,
+                if info[2] in all_ids.keys():
+                    codes = all_codes[info[2]]
+                    ids = all_ids[info[2]]
+                    first_line = self.get_first_line_num(info[0], codes, ids)
+                    message = self.get_warning_message(first_line, info[1], codes, ids)
+                    warning = RecommendationItem(project_name=self.project_root_path.stem,
                                       recommendation_code=k,
                                       file_path=path,
                                       message=message,
                                       line=first_line)
-                all_recommendations.append(warning)
+                    all_recommendations.append(warning)
                 
         return all_recommendations
