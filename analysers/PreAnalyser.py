cf77d8cec1b11be6934a4f3707086df9cbb9ccce
Yifei Zhao <77520578+Kritt430@users.noreply.github.com>
2021-08-22 22:48:15 +0800

    Update PreAnalyser.py

--- analysers/PreAnalyser.py
+++ analysers/PreAnalyser.py
@@ -4,8 +4,6 @@ from analysers.BaseAnalyser import BaseAnalyser
 from auto_editor.StructuredProjectSource import StructuredProjectSource
 from enums import WarningItem
 
-import re
-
 
 class PreAnalyser(BaseAnalyser):
     def get_all_warnings(self) -> List[WarningItem]:
@@ -18,25 +16,29 @@ class PreAnalyser(BaseAnalyser):
         project = StructuredProjectSource(self.project_root_path)
         warnings_dict = project.dpct_warnings_dict
         all_warnings = []
-        codes = []
-        ids = []
-        for i in project.paths_to_lines.values():
-            for j in i:
-                codes.append(j.code)
-                ids.append(j.id)       
+        all_codes = {}
+        all_ids = {}
+
+        for name, line_item in project.paths_to_lines.items():
+            for i in line_item:
+                all_codes.setdefault(name, []).append(i.code)
+                all_ids.setdefault(name, []).append(i.id)
 
         for k, v in warnings_dict.items():
             for info in v:
-                path = '/'+info[2]
-                first_line = self.get_first_line_num(info[0], codes, ids)
-                message = self.get_warning_message(first_line, info[1], codes, ids)
-                warning = WarningItem(project_name=self.project_root_path.stem,
-                                      warning_code=k,
-                                      file_path=path,
-                                      message=message,
-                                      # "DPCT1111:3: The workgroup size passed to the SYCL kernel may exceed the limit.\n"
-                                      #         "To get the device limit, query info::device::max_work_group_size. Adjust the\n"
-                                      #         "workgroup size if needed.",
-                                      line=first_line)
-                all_warnings.append(warning)
+                path = '/' + info[2]
+                if info[2] in all_ids.keys():
+                    codes = all_codes[info[2]]
+                    ids = all_ids[info[2]]
+                    first_line = self.get_first_line_num(info[0], codes, ids)
+                    message = self.get_warning_message(first_line, info[1], codes, ids)
+                    warning = WarningItem(project_name=self.project_root_path.stem,
+                                          warning_code=k,
+                                          file_path=path,
+                                          message=message,
+                                          # "DPCT1111:3: The workgroup size passed to the SYCL kernel may exceed the limit.\n"
+                                          #         "To get the device limit, query info::device::max_work_group_size. Adjust the\n"
+                                          #         "workgroup size if needed.",
+                                          line=first_line)
+                    all_warnings.append(warning)
         return all_warnings
