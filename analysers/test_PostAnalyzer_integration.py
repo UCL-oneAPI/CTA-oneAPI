87b4640b5a97156fd317ca4ec1a3c14814e8e8b1
Yifei Zhao <ucaby46@ucl.ac.uk>
2021-08-23 10:38:50 +0800

    update analysers

--- analysers/test_PostAnalyzer_integration.py
+++ analysers/test_PostAnalyzer_integration.py
@@ -14,15 +14,25 @@ class TestPreAnalyzerIntegration(BaseIntegrationTest):
         p = Path.joinpath(cta_root, 'auto_editor', 'sample_data', 'destination_dir')
         print("cta_root:",p)
         post_analyser = PostAnalyser(p)
-
+        final_warnings = post_analyser.get_all_warnings()
         recommendations = post_analyser.get_all_recommendation()
         print(recommendations)
+        self.assertEqual(len(final_warnings), 57)
+        self.assertEqual(final_warnings[0].project_name, 'destination_dir')
+        self.assertEqual(final_warnings[0].warning_code, 'DPCT1003')
+        self.assertEqual(final_warnings[0].file_path, '/cluster.dp.cpp')
+        self.assertEqual(final_warnings[0].message, "DPCT1003:28: Migrated API does not return error code. (*, 0) is inserted. You\n"
+                                                    "may need to rewrite this code.")
 
-        self.assertEqual(len(recommendations), 2)
+        self.assertEqual(len(recommendations), 1)
         self.assertEqual(recommendations[0].project_name, 'destination_dir')
-        self.assertEqual(recommendations[0].recommendation_code, 'CTA1003')
-        self.assertEqual(recommendations[0].file_path, '/cluster.dp.cpp')
-        self.assertEqual(recommendations[0].message,'CTA1003:1: This is the test recommendation for CTA system.')
+        self.assertEqual(recommendations[0].recommendation_code, 'CTA1065')
+        self.assertEqual(recommendations[0].file_path, '/kernels.dp.cpp')
+        self.assertEqual(recommendations[0].message,"CTA1065:0: CTA recommended to ignore this warning.\n"
+                                                    "but you can also consider replacing 'item_ct1.barrier();'\n"
+                                                    "with 'item_ct1.barrier(sycl::access::fence_space::local_space);'\n"
+                                                    "to have have better performance if the kernel function\n"
+                                                    "has no memory accesses in the global memory.")
         #self.assertEqual(recommendations[0].line, 247)
         pass
 
