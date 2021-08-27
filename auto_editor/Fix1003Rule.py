006e5068a4eb90696b1a0d09d319572ade9dfab4
Wenqi997 <1125377852@qq.com>
2021-08-27 12:53:18 +0800

    fix 1003

--- auto_editor/Fix1003Rule.py
+++ auto_editor/Fix1003Rule.py
@@ -4,9 +4,14 @@ from auto_editor.BaseRule import BaseRule
 from auto_editor.LineItem import LineItem
 from auto_editor.StructuredProjectSource import StructuredProjectSource
 from enums import ChangeTypeEnum
+from auto_editor.utils import get_index_of_line_id
 
 
 class Fix1003Rule(BaseRule):
+    def __init__(self):
+        super().__init__()
+        self.all_lines = []
+
     @property
     def dpct_warning_codes(self) -> List[str]:
         return ["DPCT1003"]
@@ -21,25 +26,27 @@ class Fix1003Rule(BaseRule):
         first_time = True
         warning_code, prefix = "", ""
         tmp_dict = project.paths_to_lines
-        all_lines = tmp_dict[file_path]
+        self.all_lines = tmp_dict[file_path]
+        warning_begin_id = self.all_lines[warning_first_line].id
+        warning_end_id = self.all_lines[warning_last_line].id
 
-        k = self.find_true_originline_number(all_lines,warning_last_line)
+        k = self.find_true_originline_number(self.all_lines,warning_last_line)
         one_line_warning_code = True
 
-        start = self.find_true_originline_number(all_lines,warning_first_line)
+        start = self.find_true_originline_number(self.all_lines,warning_first_line)
         # self.remove_code(warning_first_line,warning_last_line)
-        #self.replace_useless_multiple_line(start-1,k,all_lines)
+        # self.replace_useless_multiple_line(start-1,k,all_lines)
 
-        for i in range(k+1, len(all_lines)):
-            print("warning code:",all_lines[i].code)
-            if ";" not in all_lines[i].code:
-                now_code = self.strip_determin(first_time, all_lines[i])
+        for i in range(k+1, len(self.all_lines)):
+            print("warning code:",self.all_lines[i].code)
+            if ";" not in self.all_lines[i].code:
+                now_code = self.strip_determin(first_time, self.all_lines[i])
                 warning_code = warning_code + now_code
                 warning_code = warning_code.replace("\n", "")
                 first_time = False
                 one_line_warning_code = False
             else:
-                now_code = self.strip_determin( first_time, all_lines[i])
+                now_code = self.strip_determin( first_time, self.all_lines[i])
                 if one_line_warning_code == False:
                     warning_code = warning_code + now_code + "\n"
                 else:
@@ -48,11 +55,12 @@ class Fix1003Rule(BaseRule):
                 new_code = prefix + new_code
                 print("new_code:",new_code)
 
-                i_position = self.replace_useless_multiple_line(start-1, i, all_lines)
+                #i_position = self.replace_useless_multiple_line(start-1, i, self.all_lines)
+                i_position = self.delete_dpct_warning(warning_begin_id, warning_end_id)
                 new_lineItem = LineItem(new_code)
-                all_lines.insert(i_position,new_lineItem)
-                self.test_print( all_lines, warning_last_line)
-                tmp_dict[file_path] = all_lines
+                self.all_lines.insert(i_position,new_lineItem)
+                self.test_print( self.all_lines, warning_last_line)
+                tmp_dict[file_path] = self.all_lines
                 break
 
         project.paths_to_lines[file_path] = tmp_dict[file_path]
@@ -75,6 +83,16 @@ class Fix1003Rule(BaseRule):
     def replace_useless_multiple_line(self,k,i,all_lines):
         del all_lines[k+1:i+1]
         return k+1
+
+    def remove_code(self, first_line: int, last_line: int = None):
+        remaining_line = last_line + 1 if last_line else first_line + 1
+        del self.all_lines[first_line: remaining_line]
+
+    def delete_dpct_warning(self, warning_begin_id, warning_end_id):
+        warning_begin_i = get_index_of_line_id(warning_begin_id, self.all_lines)
+        warning_end_i = get_index_of_line_id(warning_end_id, self.all_lines)
+        self.remove_code(warning_begin_i, warning_end_i)
+        return warning_end_i
 
     # def get_index_of_line_id(line_id, code_lines: List[LineItem]) -> int:
     #     for i in range(len(code_lines)):
