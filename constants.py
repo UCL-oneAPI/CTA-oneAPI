4c18d53256886a762b629ccb983ed31759068521
costa-e <ehrensberger.c@me.com>
2021-09-01 14:01:26 +0100

    remove redundant add comments rule call

--- constants.py
+++ constants.py
@@ -7,8 +7,6 @@ RULES_TO_DESCRIPTIONS = {
     Fix1049Rule: "Reproduce 1049 manual changes from sample data",
     AddCommentsRule: "Insert comments to provide recommendations for various warnings",
 
-    AddCommentsRule: "Insert comments to provide recommendations for various warnings",
-
     # mock rule with simple exemplary implementation
     # AddCommentsTestRule: "Insert comments to provide recommendations for various warnings"
 }
